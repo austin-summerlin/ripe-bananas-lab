5dc531a6c40cff629f1ee3b9d6f09a747ef31979
Katherine Tam <katherinemtam@gmail.com>
2021-06-25 15:47:03 -0700

    get (by id) route test

--- __tests__/actor.test.js
+++ __tests__/actor.test.js
@@ -40,6 +40,7 @@ describe.only('Actor routes', () => {
 
 
   });
+
   it('get all actors via GET', async () => {
     await Actor.create(actor1);
     await Actor.create(actor2);
@@ -51,6 +52,19 @@ describe.only('Actor routes', () => {
     expect(res.body).toEqual(expected);
   });
 
+  it('get an actor via GET', async () => {
+    const actor = await Actor.create(actor1);
+
+    const res = await request(app)
+      .get(`/api/v1/actors/${actor.id}`);
+
+    expect(res.body).toEqual({
+      ...actor.toJSON(),
+      dob: actor.dob.toISOString(),
+      updatedAt: actor.updatedAt.toISOString(),
+      createdAt: actor.createdAt.toISOString()
+    });
+  });
 
 });
 
