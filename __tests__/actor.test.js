415b87c36e26685436f692f7274dd9e709da6655
Austin Summerlin <summerlin.austin@gmail.com>
2021-06-25 15:13:18 -0700

    get all actors red test

--- __tests__/actor.test.js
+++ __tests__/actor.test.js
@@ -3,6 +3,18 @@ import request from 'supertest';
 import app from '../lib/app.js';
 import Actor from '../lib/models/Actor.js';
 
+const actor1 = {
+  name: 'Bean Benny B',
+  dob: '1969-04-20',
+  pob: 'Coolzone, USA'
+};
+
+const actor2 = {
+  name: 'Tony Bologna',
+  dob: '1904-03-11',
+  pob: 'Brooklyn, USA'
+};
+
 describe.only('Actor routes', () => {
   beforeEach(() => {
     return db.sync({ force: true });
@@ -25,8 +37,20 @@ describe.only('Actor routes', () => {
       updatedAt: expect.any(String),
       createdAt: expect.any(String)
     });
-    
+
 
   });
+  it('get all actors via GET', async () => {
+    await Actor.create(actor1);
+    await Actor.create(actor2);
+
+    const expected = [actor1, actor2];
+
+    const res = await request(app)
+      .get('/api/v1/actors');
+    expect(res.body).toEqual(expected);
+  });
+
 
 });
+
