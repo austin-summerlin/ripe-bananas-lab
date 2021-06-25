7f165b0d4c4e7344336649d8df9b3b55b7b27cc9
Austin Summerlin <summerlin.austin@gmail.com>
2021-06-24 17:17:21 -0700

    red post test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,32 @@
-import pool from '../lib/utils/pool.js';
-import setup from '../data/setup.js';
+import db from '../lib/utils/db.js';
+
 import request from 'supertest';
 import app from '../lib/app.js';
 
 describe('demo routes', () => {
   beforeEach(() => {
-    return setup(pool);
+    return db.sync({ force: true });
   });
+
+  it('creates a studio via POST', async () => {
+    const res = await request(app)
+      .post('/api/v1/studios')
+      .send({
+        name: 'TA Station',
+        city: 'Portland',
+        state: 'Oregon',
+        country: 'US'
+      });
+
+    expect(res.body).toEqual({
+      id: 1,
+      name: 'TA Station',
+      city: 'Portland',
+      state: 'Oregon',
+      country: 'US',
+      updatedAt: expect.any(String),
+      createdAt: expect.any(String)
+    });
+  });
+
 });
