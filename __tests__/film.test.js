d0f11a3d76fa674acb6b2412abe3bf1207e12595
Tis Lais <tistististis@gmail.com>
2021-06-29 10:21:10 -0700

    Add failing get all films test

--- __tests__/film.test.js
+++ __tests__/film.test.js
@@ -2,11 +2,25 @@ import db from '../lib/utils/db.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 import Film from '../lib/models/Film.js';
+import Studio from '../lib/models/Studio.js';
 
 const film1 = {
   title: 'Revenge of the Beans',
   studio: 1,
   released: 2021
+};
+
+const film2 = {
+  title: 'Revenge of the Beans 2: Electric Boogaloo',
+  studio: 1,
+  released: 2022
+};
+
+const taStation = {
+  name: 'TA Station',
+  city: 'Portland',
+  state: 'Oregon',
+  country: 'US'
 };
 
 describe('Film routes', () => {
@@ -25,6 +39,39 @@ describe('Film routes', () => {
       updatedAt: expect.any(String),
       createdAt: expect.any(String)
     });
+  });
+
+  it('gets all films via GET', async () => {
+    await Studio.create(taStation);
+    await Film.create(film1);
+    await Film.create(film2);
+
+    const res = await request(app)
+      .get('/api/v1/films');
+
+    const expected = [
+      {
+        id: 1,
+        title: 'Revenge of the Beans',
+        released: 2021,
+        studio: {
+          id: 1,
+          name: 'TA Station'
+        }
+      },
+      {
+        id: 2,
+        title: 'Revenge of the Beans 2: Electric Boogaloo',
+        released: 2022,
+        studio: {
+          id: 1,
+          name: 'TA Station'
+        }
+      }
+    ];
+
+    expect(res.body).toEqual(expected);
+
   });
 
 });
